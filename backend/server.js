e6b976c35e4bfd5cba6cf4ed6cde7754afe31385
gokulrajs098 <serajgokul19@gmail.com>
2025-08-07 12:09:59 +0530

    just added backend structure

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+
+const port = 5000;
+
+app.use(express.json());
+
+app.get('')
+
+app.listen(port, ()=>{
+    console.log("server is running on port ${port}");
+})
